f6e2b9e826d0b14a226d3575167238dbdf8711e6
Boyko Karadzhov <boyko@payhawk.io>
2019-06-07 17:54:39 +0300

    verified unit tests infrastructure

--- src/dummy.spec.ts
+++ src/dummy.spec.ts
@@ -2,8 +2,4 @@ describe('Unit tests infrastructure', () => {
     test('runs tests and updates badge', async () => {
         expect(true).toBe(true);
     });
-
-    test('fail on purpose!', async () => {
-        expect(true).toBe(false);
-    });
 });
