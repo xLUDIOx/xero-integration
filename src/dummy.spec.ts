f7925fb22791fabc27c5a4fff2b82cb6b455a502
Boyko Karadzhov <boyko@payhawk.io>
2019-06-07 17:51:59 +0300

    test docker build

--- src/dummy.spec.ts
+++ src/dummy.spec.ts
@@ -2,4 +2,8 @@ describe('Unit tests infrastructure', () => {
     test('runs tests and updates badge', async () => {
         expect(true).toBe(true);
     });
+
+    test('fail on purpose!', async () => {
+        expect(true).toBe(false);
+    });
 });
