ed0ba9800e09492d95f80855ba6dd649d76b0ae6
vasil.dinev <vasil.dinev@payhawk.io>
2019-10-15 10:35:03 +0300

    expose port

--- swap.sh
+++ swap.sh
@@ -7,6 +7,6 @@ docker build . -f ./Dev.Dockerfile -t adapters-xero-dev || exit 1
 telepresence  --mount=/tmp/xero \
     --namespace adapters \
     --swap-deployment xero-integration --expose 8080 \
-    --docker-run --rm -v "${PWD}/build:/app/build" -p=9230:9230 \
+    --docker-run --rm -v "${PWD}/build:/app/build" -p 8040:8080 -p=9230:9230 \
     -e TELEPRESENCE_MOUNT_PATH="/tmp/xero" \
     -it adapters-xero-dev
