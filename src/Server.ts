25652aa56f39889e996bd2a6ee1d3986a0b7e3e1
Boyko Karadzhov <boyko@payhawk.io>
2020-03-18 21:53:31 +0200

    xero security headers

--- src/Server.ts
+++ src/Server.ts
@@ -3,12 +3,27 @@ import * as restify from 'restify';
 import { config } from './Config';
 import { Controller } from './controller';
 
+function securityHeadersMiddleware(): restify.RequestHandler {
+    return (request: restify.Request, response: restify.Response, next: restify.Next) => {
+        response.set({
+            'X-Frame-Options': 'DENY',
+            // cspell:disable-next
+            'X-Content-Type-Options': 'nosniff',
+            'X-XSS-Protection': '1; mode=block',
+            'X-Permitted-Cross-Domain-Policies': 'none',
+            'Content-Security-Policy': `default-src 'none'`,
+        });
+        next();
+    };
+}
+
 export const createServer = (controller: Controller): restify.Server => {
     const server = restify.createServer({ name: config.serviceName });
 
     server
         .use(restify.plugins.jsonBodyParser())
-        .use(restify.plugins.queryParser());
+        .use(restify.plugins.queryParser())
+        .use(securityHeadersMiddleware());
 
     // Endpoint used to check whether the service is up and running
     server.get('/status', (req, res) => res.send(200, 'OK'));
