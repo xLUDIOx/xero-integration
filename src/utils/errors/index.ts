5498d0f0baf906a46f17571075fb0c403da29e28
Vasil Dinev <vassildinev@gmail.com>
2021-01-07 17:07:05 +0200

    Fix error message (#101)

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -7,4 +7,4 @@ export const ARCHIVED_ACCOUNT_CODE_MESSAGE_REGEX = /Account code '.+' has been a
 export const ARCHIVED_BANK_ACCOUNT_MESSAGE_REGEX = /.+ bank account is archived and cannot be used/;
 export const LOCK_PERIOD_ERROR_MESSAGE = 'Cannot export expense into Xero because the service period is locked for this organisation';
 export const EXPENSE_RECONCILED_ERROR_MESSAGE = 'Cannot export expense into Xero because it has been reconciled';
-export const DEFAULT_ACCOUNT_CODE_ARCHIVED_ERROR_MESSAGE = 'Default expense account is required, but it is currently of status \'ARCHIVED\'';
+export const DEFAULT_ACCOUNT_CODE_ARCHIVED_ERROR_MESSAGE = 'Default expense account is required but it is currently of status \'ARCHIVED\'';
